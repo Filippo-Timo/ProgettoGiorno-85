911c0835383980957247b22e08c78e2e5786731f
Filippo-Timo <filtimo2002@gmail.com>
2026-02-13 10:05:11 +0100

    Initial commit

---
+++ src/main/java/filippotimo/ProgettoGiorno_85/ProgettoGiorno85Application.java
@@ -0,0 +1,13 @@
+package filippotimo.ProgettoGiorno_85;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoGiorno85Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoGiorno85Application.class, args);
+	}
+
+}
---
+++ src/test/java/filippotimo/ProgettoGiorno_85/ProgettoGiorno85ApplicationTests.java
@@ -0,0 +1,13 @@
+package filippotimo.ProgettoGiorno_85;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoGiorno85ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
